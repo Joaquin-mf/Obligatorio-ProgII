4e8c8177b4c6613aadb5e1221f649e42c6861d2b
Juan <juan@Juans-MacBook-Pro-3.local>
2024-05-19 17:26:58 -0300

    hice tests para binary tree

--- test/binaryTreeTest.java
+++ test/binaryTreeTest.java
@@ -1,10 +1,11 @@
 import org.junit.Test;
 import uy.edu.um.adt.binarytree.MySearchBinaryTree;
 import uy.edu.um.adt.binarytree.MySearchBinaryTreeImpl;
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.*;
 
 
-public class binaryTreeTest {
+public class binaryTreeTest { //getRoot, getLeft, getRight, getKey, add, remove, contains, find, inOrder, PostOrder, PreOrder
     @Test
     public void tests(){
         MySearchBinaryTree<Integer,String> arbol = new MySearchBinaryTreeImpl<Integer, String>();
@@ -13,14 +14,17 @@ public class binaryTreeTest {
         arbol.add(30,"tercero");
         arbol.add(13,"cuarto");
         arbol.add(5,"quinto");
-        arbol.add(13,"sexto");
+        arbol.add(18,"sexto");
         arbol.add(20,"septimo");
 
         assertEquals((Integer) 15, arbol.getRoot().getKey());
         assertEquals((Integer) 5, arbol.getRoot().getLeft().getLeft().getKey());
         assertEquals((Integer) 20, arbol.getRoot().getRight().getLeft().getKey());
-
+        assertTrue(arbol.contains(30));
         arbol.remove(30);
+        assertFalse(arbol.contains(30));
+        assertEquals("segundo", arbol.find(10));
+
 
 
     }
