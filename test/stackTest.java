7166acc8f730bc721cffc8e53dcc24b489be245a
Juan <juan@Juans-MacBook-Pro-3.local>
2024-05-19 16:18:48 -0300

    hice los tests para el stack

--- test/stackTest.java
+++ test/stackTest.java
@@ -1,9 +1,59 @@
+import com.sun.jdi.IntegerValue;
 import org.junit.Test;
+import uy.edu.um.adt.linkedlist.MyLinkedListImpl;
+import uy.edu.um.adt.stack.EmptyStackException;
+import uy.edu.um.adt.stack.MyStack;
 
-public class stackTest {
+import static org.junit.Assert.assertEquals;
+
+public class stackTest {  // Push,Pop,Peek,size
+
 
     @Test
-    public void stackTest(){
+    public void stackTestSize(){
+        MyStack<Integer> stack = new MyLinkedListImpl<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+
+        assertEquals(4, stack.size());
+    }
+    @Test
+    public void stackTestPush(){
+        MyStack<Integer> stack = new MyLinkedListImpl<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        assertEquals(Integer.valueOf(4), stack.peek());
 
     }
+
+    @Test
+    public void stackTestPop(){
+        MyStack<Integer> stack = new MyLinkedListImpl<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        try {
+            assertEquals(Integer.valueOf(4), stack.pop());
+        } catch (EmptyStackException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void stackTestPeek(){
+        MyStack<Integer> stack = new MyLinkedListImpl<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+
+        assertEquals(Integer.valueOf(4), stack.peek());
+    }
+
 }
+
