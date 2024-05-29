35195db91676f7c8859cc1c1a1af7b893c741e70
joaquinmartirena <jmartirenaf@gmail.com>
2024-05-29 08:42:35 -0300

    HashDateArtist

--- src/StorageData/HashArtistDate.java
+++ src/StorageData/HashArtistDate.java
@@ -14,18 +14,16 @@ public class HashArtistDate {
         MyHash<String, MyList<SpotifySong>> hash = new MyHashImpl<>(113);
         for (int i = 0; i < songsList.size(); i++) {
             SpotifySong song = songsList.get(i);
-            for (int j = 0; j < song.getArtists().size(); j++)
-                String songKey = generateKey(song.getSnapshotDate(), song.getArtists().get(j));
-
-//            if(!hashDateCountry.contains(songKey)){
-//                hashDateCountry.put(songKey, new MyLinkedListImpl<>());
-//            }
-//            hashDateCountry.findNode(songKey).getData().add(song);
-//        }
-//        return hashDateCountry;
-        }
-    }
-    public static String generateKey (LocalDate date, Artists artists){
-                return date.toString() + "_" + artists.getName();
+            LocalDate dia = song.getSnapshotDate();
+            for (int j = 0; j < song.getArtists().size(); j++) {
+                Artists persona = song.getArtists().get(j);
+                String songKey = song.getSnapshotDate().toString() + "_" + persona.getName();
+                if (!hash.contains(songKey)) {
+                    hash.put(songKey, new MyLinkedListImpl<>());
+                }
+                hash.findNode(songKey).getData().add(song);
             }
+        }
+        return hash;
+    }
 }
